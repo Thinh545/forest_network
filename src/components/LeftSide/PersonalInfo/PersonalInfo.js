aabcd232f947f8bbb0928d958c4b36ecf146b916
NGUYENDUYTHANG <NguyenDuyThang@bitbucket.org>
2018-11-30 14:50:48 +0700

    Create input fieldsor edit

--- src/components/LeftSide/PersonalInfo/PersonalInfo.js
+++ src/components/LeftSide/PersonalInfo/PersonalInfo.js
@@ -1,36 +1,80 @@
 import React, { Component } from 'react';
-import { Layout, Icon } from 'antd';
+import { Layout, Icon, Input } from 'antd';
 import './PI.css';
 
 const { Content } = Layout;
 
 class PersonalInfo extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            enableEdit: true,
+        }
+    }
 
     render(){
         return(
             <Content style={styles.wrapper}>
-                <Content style = {styles.username}>
+                {!this.state.enableEdit && <Content style = {styles.username}>
                     Youtube
-                </Content>
+                </Content>}
+               {this.state.enableEdit && <Input
+                    style = {styles.username}
+                    value={'Yotube'}
+                />}
+
                 <Content style = {{marginBottom: 10}}>
                     <a href = '@Youtube'>@Youtube</a>
                 </Content>
-                <Content style = {{color: 'black'}}>
+
+                {!this.state.enableEdit && <Content style = {{color: 'black'}}>
                     Imagine if you couldn’t watch the videos you love. We support copyright reform with an Article 13 that works for everyone. #SaveYourInternet
-                </Content>
-                <Content style = {styles.joinDate}>
+                </Content>}
+                {this.state.enableEdit && <Content style = {{color: 'black'}}>
+                    Description
+                    <Input
+                        style = {{color: 'black'}}
+                        value = 'Imagine if you couldn’t watch the videos you love. We support copyright reform with an Article 13 that works for everyone. #SaveYourInternet'
+                    />
+                </Content>}
+
+                {!this.state.enableEdit && <Content style = {styles.joinDate}>
                     <Icon type="environment" style = {{marginRight: 5}}/>
                     San Bruno, CA
-                </Content>
-                <Content style = {styles.joinDate}>
+                </Content>}
+                {this.state.enableEdit && <Content style = {styles.joinDate}>
+                    <Icon type="environment" style = {{marginRight: 5}}/>
+                    <Input
+                        style = {{marginRight: 5, color: 'black'}}
+                        value = 'San Bruno, CA'
+                    />
+                </Content>}
+
+                {!this.state.enableEdit && <Content style = {styles.joinDate}>
                     <Icon type="link" style = {{marginRight: 5}}/>
                     <a href = 'https://www.youtube.com'>https://www.youtube.com</a>
-                </Content>
-                <Content style = {styles.joinDate}>
+                </Content>}
+                {this.state.enableEdit && <Content style = {styles.joinDate}>
+                    <Icon type="link" style = {{marginRight: 5}}/>
+                    <Input
+                        style = {{marginRight: 5, color: 'black'}}
+                        value = 'https://www.youtube.com'
+                    />
+                </Content>}
+
+                {!this.state.enableEdit && <Content style = {styles.joinDate}>
                     <Icon type="calendar" style = {{marginRight: 10}}/>
                     Joined November 2007
-                </Content>
-                <Content style = {{marginTop: 20}}>
+                </Content>}
+                {this.state.enableEdit && <Content style = {styles.joinDate}>
+                    <Icon type="calendar" style = {{marginRight: 10}}/>
+                    <Input
+                        style = {{marginRight: 10, color: 'black'}}
+                        value = 'Joined November 2007'
+                    />
+                </Content>}
+
+                {!this.state.enableEdit &&  <Content style = {{marginTop: 20}}>
                     <Content
                         style = {
                             {
@@ -57,7 +101,7 @@ class PersonalInfo extends Component{
                         className = 'image'
                         src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxadr9ykSPoaet-5e7-_YZtueYaRJSvggWtEShh2EJyAjAf5-D'
                     />
-                </Content>
+                </Content>}
             </Content>
         );
     }
