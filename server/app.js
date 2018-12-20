5a9e8a45aeb8a5d33c8465e30b3ed5616c07ec6b
Thinh545 <nguyenhuuthinh1997@gmail.com>
2018-12-20 23:30:42 +0700

    fix heroku 503

--- server/app.js
+++ server/app.js
@@ -35,4 +35,9 @@ mongoose
 
 app.use('/account', accountRouter);
 
+var server = app.listen(process.env.PORT || 5000, function () {
+    var port = server.address().port;
+    console.log("Express is working on port " + port);
+});
+
 module.exports = app;
