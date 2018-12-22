7f99cd93f63abe70fd6a477bff84204d98dc24b1
ngtritham <ngtritham@gmail.com>
2018-12-23 02:15:55 +0700

    create account endpoint

--- server/controllers/accountControllers.js
+++ server/controllers/accountControllers.js
@@ -2,6 +2,9 @@
 const axios = require('axios');
 const _ = require('lodash');
 const moment = require('moment');
+const crypto = require('crypto');
+const { Keypair, sign } = require('stellar-base');
+const key = Keypair.random();
 
 // Models
 const accountModel = require('./../models/accountModel');
@@ -17,8 +20,9 @@ const {
 } = require('./../configs/env');
 
 // Lib
-const v1 = require('../lib/tx/v1');
-
+const v1 = require('./../lib/tx/v1');
+// const transaction = require('./../lib/tx/index');
+const transaction = require('./../lib/tx/index');
 
 module.exports = {
     getInfo: async (req, res) => {
@@ -49,6 +53,8 @@ module.exports = {
                     let buf = Buffer.from(trans.tx, 'base64');
                     let data_trans = v1.decode(buf);
 
+                    console.log(data_trans);
+
                     if (data_trans.operation == 'payment') {
                         if (data_trans.account === public_key) {
                             account.balance -= data_trans.params.amount;
@@ -61,10 +67,6 @@ module.exports = {
                         account.sequence = data_trans.sequence;
                     }
                 });
-
-                // // Tính năng lượng
-                // let account_bandwidth = amount * system_bandwidth / max_cel - used_bandwidth;
-                // let used_bandwidth = current_bandwidth * MAX(0, (period - t)/(period));
 
                 res.status(200);
                 data_return.status = 200;
@@ -135,5 +137,77 @@ module.exports = {
         }
 
         res.json(data_return);
+    },
+
+    // createAccount: (req, res) => {
+    //     // const tx = req.body.tx;
+    //     // console.log(tx);
+    //     let tx = {
+    //         version: 1,
+    //         account : 'GDQVHLWFQW3M6H23J2IMRMBK4MHZ5ZF56LJWN7WNXPAMRTL5MQPVCPKL',
+    //         sequence: 1,
+    //         memo: Buffer.from('Test create account'),
+    //         operation: 'create_account',
+    //         params: {
+    //             address: 'GDCRVBWSBCUP74I7FWXBIX56QCXTDAKD4CZQDBUTM7CKYAI2BOGSYUAP'
+    //         },
+    //     }
+
+    //     // const data = transaction.encode(tx);
+    //     // const data = transaction.encode(tx).toString('hex');
+    //     // const unsignedHash = crypto
+    //     // .createHash('sha256')
+    //     // .update(data)
+    //     // .digest();
+    //     transaction.sign(tx, 'SCRC7SUP5WKJLEFWCUSBBLJ7RI2TCDLA5O6Q4BWBFW2M57JNTCVN3LIO')
+    //     const data = '0x' + transaction.encode(tx).toString('hex');
+
+    //     // const commit = axios.default.post()
+    //     console.log(data);
+    //     res.send(data);
+    // },
+
+    getCreateParams: (req, res) => {
+        const account = req.query.account;
+        const public_key = req.query.public_key;
+
+        let data_return = {
+            status: 500,
+            msg: 'Unexpected Error',
+            data: {}
+        }
+
+        if (_.isEmpty(account) || !_.isString(account)) {
+            res.status(400)
+            data_return.status = 400;
+            data_return.msg = 'Invalid <account> string !'
+        } else if(_.isEmpty(public_key) || !_.isString(public_key)) {
+            res.status(400)
+            data_return.status = 400;
+            data_return.msg = 'Invalid <public_key> string !'
+        } else {
+            const tx = {
+                version: 1,
+                account : account,
+                sequence: 1,
+                memo: Buffer.from('Create account'),
+                operation: 'create_account',
+                params: {
+                    address: public_key
+                },
+            }
+
+            res.status(200);
+            data_return.status = 200;
+            data_return.msg = 'OK !';
+            data_return.data = tx;
+        }
+
+        res.json(data_return);
+    },
+
+    postCreateCommit: async (req, res) => {
+        const tx = req.body.tx;
+        res.send(tx);
     }
 }
